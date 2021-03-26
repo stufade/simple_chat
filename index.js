11a607a770cfa1621077ea938957b3a82f7c3fce
stufade <stufade223@gmail.com>
2021-03-27 00:06:04 +0300

    Private messages + notifications

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-let usersCodes = {};
+let usersCodes = {}; // socket.id -> {color, userId}
+let usersIds = {};   // userId -> socket.id
 let userCode = 1;
 
 app.use(express.static("public"));
@@ -15,21 +16,30 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log(`User ${userCode} connected\n`);
-  usersCodes[socket.id] = new User(userCode++);
+  usersCodes[socket.id] = new User(userCode);
+  usersIds[userCode] = socket.id;
+  io.emit("chat message", `User ${userCode} connected`, usersCodes[socket.id].color, 0);
   console.log(usersCodes);
-  console.log("\n")
+  console.log("\n");
+  userCode++;
+
+  socket.on('private message', (recipient, msg) => {
+    io.to(usersIds[recipient]).emit('chat message', msg, usersCodes[socket.id].color, 2, "private");
+    socket.emit('chat message', msg, usersCodes[socket.id].color, 1, "private");
+  });
 
   socket.on('chat message', (msg) => {
     socket.emit('chat message', msg, usersCodes[socket.id].color, 1);
     socket.broadcast.emit('chat message', msg, usersCodes[socket.id].color, 2);
   });
 
-  socket.on('newUser', () => {
+  socket.on('newMessage', () => {
     console.log(`User ${usersCodes[socket.id].userId} sent a message\n`);
   });
 
   socket.on("disconnect", () => {
     console.log(`User ${usersCodes[socket.id].userId} disconnected\n`);
+    io.emit("chat message", `User ${usersCodes[socket.id].userId} disconnected`, usersCodes[socket.id].color, 0);
 
     delete usersCodes[socket.id];
 
--- public/logic.js
+++ public/logic.js
@@ -4,23 +4,39 @@ const messageWindow = document.querySelector(".window");
 
 input.addEventListener("keydown", function(e) {
     if (e.key == "Enter") {
+        let text = input.value.trim();
         e.preventDefault();
-        if (!input.value.trim()) return;
-        socket.emit("newUser");
-        socket.emit('chat message', input.value.trim());
+        if (!text) return;
+
+        let words = text.split(" ");
+        if (words[0] == "/w" && words[1]) {
+            socket.emit("private message", words[1], words.slice(2).join(" "));
+            input.value = "";
+            return;
+        }
+
+        socket.emit("newMessage");
+        socket.emit('chat message', text);
         input.value = "";
     }
 });
 
-socket.on('chat message', function(msg, color, messageId) {
-    let message = createMessage(msg, color, messageId);
+socket.on('chat message', function(msg, color, messageId, additionalClasses = "") {
+    let message = createMessage(msg, color, messageId, additionalClasses);
     messageWindow.prepend(message);
 });
 
-function createMessage(msg, color, messageId) {
+function createMessage(msg, color, messageId, additionalClasses) {
     let div = document.createElement("div");
     div.classList.add(`message${messageId}`);
-    div.style.backgroundColor = color;
+    if (additionalClasses) {
+        div.classList.add(additionalClasses);
+    }
+    if (messageId === 0) {
+        div.style.color = color;
+    } else {
+        div.style.backgroundColor = color;
+    }
     div.textContent = msg;
     return div;
 }
