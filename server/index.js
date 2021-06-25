4839921fe81a9f064a036b40391c7b5b2adde16c
Stufade <stufade223@gmail.com>
2021-06-25 17:52:54 +0300

    Delete useless import

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { copyFileSync } = require('fs');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
